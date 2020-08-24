22bffa92b6fce51c652d7a06ef6485d9ab2fbcd3
Liam Beeton <liam.beeton@gmail.com>
2020-08-24 14:33:50 +0200

    feat(services): navigate tabs and text

--- src/components/services.js
+++ src/components/services.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Button, Col, Container, Nav, Row } from "react-bootstrap"
+import { Button, Col, Container, Nav, Row, Tab } from "react-bootstrap"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faGraduationCap } from "@fortawesome/free-solid-svg-icons"
 import scrollTo from "gatsby-plugin-smoothscroll"
@@ -9,43 +9,85 @@ const Services = () => {
   return (
     <Container fluid className="services-wrapper p-5" id="services">
       <h1 className="text-center">Our Services</h1>
-      <Row noGutters>
-        <Col>
-          <Nav justify variant="pills" defaultActiveKey="/home" as="ul" className="mt-5">
-            <Nav.Item as="li">
-              <Nav.Link eventKey="link-1"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
-              <h3>Learning Needs Analysis</h3>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link eventKey="link-2"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
-              <h3>Programme Design &amp; Development</h3>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link eventKey="link-3"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
-              <h3>Course &amp; Content Development</h3>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link eventKey="link-4"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
-              <h3>e-Learning Strategy &amp; Implementation</h3>
-            </Nav.Item>
-            <Nav.Item as="li">
-              <Nav.Link eventKey="link-5"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
-              <h3>Facilitation Training</h3>
-            </Nav.Item>
-          </Nav>
-        </Col>
-      </Row>
-      <Row noGutters>
-        <Col>
-          <blockquote className="blockquote text-center">
-            <p className="mb-0">
-              The key to instilling a culture of lifelong learning is to curate and personalise flexible learning experiences. By gaining
-              an individual understanding of each individual’s baseline and desired skills and knowledge, as well as preferred learning
-              styles and the needs of the business, you’ll be in a better position meet those needs.
-						</p>
-          </blockquote>
-        </Col>
-      </Row>
+      <Tab.Container defaultActiveKey="tab-1" id="tabs">
+        <Row noGutters>
+          <Col>
+            <Nav justify variant="pills" as="ul" className="mt-5">
+              <Nav.Item as="li">
+                <Nav.Link eventKey="tab-1"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
+                <h3>Learning Needs Analysis</h3>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <Nav.Link eventKey="tab-2"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
+                <h3>Programme Design &amp; Development</h3>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <Nav.Link eventKey="tab-3"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
+                <h3>Course &amp; Content Development</h3>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <Nav.Link eventKey="tab-4"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
+                <h3>e-Learning Strategy &amp; Implementation</h3>
+              </Nav.Item>
+              <Nav.Item as="li">
+                <Nav.Link eventKey="tab-5"><FontAwesomeIcon icon={faGraduationCap} size="5x" /></Nav.Link>
+                <h3>Facilitation Training</h3>
+              </Nav.Item>
+            </Nav>
+          </Col>
+        </Row>
+        <Row noGutters>
+          <Col>
+            <Tab.Content>
+              <Tab.Pane eventKey="tab-1">
+                <blockquote className="blockquote text-center">
+                  <p class="mb-0">
+                    The key to instilling a culture of lifelong learning is to curate and personalise flexible learning experiences. By gaining
+                    an individual understanding of each individual’s baseline and desired skills and knowledge, as well as preferred learning
+                    styles and the needs of the business, you’ll be in a better position meet those needs.
+                  </p>
+                </blockquote>
+              </Tab.Pane>
+              <Tab.Pane eventKey="tab-2">
+                <blockquote className="blockquote text-center">
+                  <p class="mb-0">
+                    We design interactive, user-centered learning programmes and academies that respond to the needs of your organisation. We
+                    partner with you to design a curriculum that embeds competency-based learning objectives that are linked to your organisational
+                    goals and the development needs of your teams or students.
+                  </p>
+                </blockquote>
+              </Tab.Pane>
+              <Tab.Pane eventKey="tab-3">
+                <blockquote className="blockquote text-center">
+                  <p class="mb-0">
+                    We’ll design, develop or transform your content into experiential, memorable and practical online and face-to-face learning
+                    experiences. By using integrated learner management systems and other digital technologies, learners are taken on
+                    a journey in which they move from acquisition to application, to adoption of new skills and competencies.
+                  </p>
+                </blockquote>
+              </Tab.Pane>
+              <Tab.Pane eventKey="tab-4">
+                <blockquote className="blockquote text-center">
+                  <p class="mb-0">
+                    We’ll work with you to design and implement a learner management system and an accompanying change management and communication
+                    strategy that speaks to your organisational goals and needs, and which allows you to continuously track and monitor
+                    engagement and performance.
+                  </p>
+                </blockquote>
+              </Tab.Pane>
+              <Tab.Pane eventKey="tab-5">
+                <blockquote className="blockquote text-center">
+                  <p class="mb-0">
+                    We conduct train-the-trainer, facilitation and blended learning workshops and courses to ensure your team is ready to deliver
+                    content in an engaging and memorable way. By combining face-to-face workshops with online learning modules and individualised
+                    coaching support, we’ll empower your team to deliver in-house content and programmes confidently and effectively.
+                  </p>
+                </blockquote>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
       <Row noGutters>
         <Col className="text-center">
           <Button variant="outline-primary" onClick={() => scrollTo("#about")} className="btn btn-xl mt-5 link-no-style">About Us</Button>
