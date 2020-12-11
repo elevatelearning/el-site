7062d649c3ca8a455cdb95a89222a9c3362529c5
Liam Beeton <liam.beeton@gmail.com>
2020-12-11 22:24:49 +0200

    refactor: use layout with children

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,22 +1,17 @@
 import React from "react"
 
+import SEO from "../components/seo"
 import Header from "../components/header"
-import Home from "../components/home"
-import Services from "../components/services"
-import About from "../components/about"
-import Contact from "../components/contact"
 import Footer from "../components/footer"
 
-const Layout = () => {
+const Layout = ({ children }) => {
   return (
-    <main>
+    <>
+      <SEO></SEO>
       <Header></Header>
-      <Home></Home>
-      <Services></Services>
-      <About></About>
-      <Contact></Contact>
+      <main>{children}</main>
       <Footer></Footer>
-    </main>
+    </>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import Home from "../components/home"
+import Services from "../components/services"
+import About from "../components/about"
+import Contact from "../components/contact"
 
 const Index = () => {
   return (
     <Layout>
-      <SEO></SEO>
+      <Home></Home>
+      <Services></Services>
+      <About></About>
+      <Contact></Contact>
     </Layout>
   )
 }
